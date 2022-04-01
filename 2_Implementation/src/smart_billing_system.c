4f1f12508f823f6d1e72cdb6048c288e0394d0f6
sathya-priya-k <101281756+Sathyapriyak4744@users.noreply.github.com>
2022-04-01 20:49:28 +0530

    Add files via upload

---
+++ 2_Implementation/src/smart_billing_system.c
@@ -0,0 +1,25 @@
+#include"main.h"
+{
+void gotoxy(int x,int y)
+
+COORD coord = {0,0};
+coord.X = x;
+    coord.Y = y;
+
+    void gotoxy(int x,int y)
+
+    int num;
+float price;
+float total;
+char choice;
+int again;
+
+mainmenu()
+
+ void chat()
+
+void exit(back)
+	   getch();
+  exit(0);
+}
+
